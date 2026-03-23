15ca78d3c0ae0e440e54f91c0df51d9644238199
ShyMike <122023566+ImShyMike@users.noreply.github.com>
2026-03-23 16:47:32 +0000

    fix: re add user dm on case closed

--- src/slack/handlers/actions.py
+++ src/slack/handlers/actions.py
@@ -9,7 +9,7 @@ from slack_sdk.errors import SlackApiError
 
 from src.config import CHANNEL, slack_app, slack_user_client
 from src.services.webhook_dispatcher import dispatch_event
-from src.slack.helpers import thread_manager
+from src.slack.helpers import send_dm_to_user, thread_manager
 
 
 @slack_app.action("mark_completed")  # type: ignore
@@ -36,6 +36,10 @@ def handle_mark_completed(ack: Any, body: dict[str, Any], client: WebClient) -> 
                 text=f"This thread has been closed by <@{body['user']['id']}>.",
                 icon_emoji=":information_source:",
                 username="Thread Info",
+            )
+            send_dm_to_user(
+                user_id,
+                "*Your case has been marked as completed by the fraud squad.*",
             )
             dispatch_event(
                 "thread.status.changed",
